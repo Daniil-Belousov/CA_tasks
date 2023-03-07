530324df8b2c5cb395a0c1e9c4d75a94cc197662
Daniil Belousov <d.belousov.exceedteam@gmail.com>
2023-03-07 22:13:53 +0300

    fourth task

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 const mainUrl = 'http://localhost:8080/http://localhost:3000/'
+const Form = document.getElementById('form');
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const TEL_REGEXP = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
 
-document.querySelector('#first').onclick = function(){
-  const value = document.querySelector('#searchInput').value
+function createMessage (text) {
+  const messageBlock = document.querySelector('#messageBlock');
 
-  fetch(`${mainUrl}filtered_items?text=${value}`)
-  .then(function (response) {
-    response.json().then(function (data) {
-      console.log('data.filteredItems',data.filteredItems);
-      const filteredItems = data.filteredItems;
-      const itemsWrapper = document.querySelector('.itemsWrapper')
+  while (messageBlock.firstChild) {
+    messageBlock.removeChild(messageBlock.firstChild);
+  }
 
-      while (itemsWrapper.firstChild) {
-        itemsWrapper.removeChild(itemsWrapper.firstChild);
-      }
+  let div = document.createElement('div');
 
-      filteredItems.forEach(item => {
-        let div = document.createElement('div');
-        div.className = 'item';
-        div.innerHTML = `<h1>${item.name}</h1>`;  
-        itemsWrapper.append(div);
-      })
-    })
+  div.className = 'wrap';
+  div.innerHTML = `<h1>${text}</h1>`;  
+  messageBlock.append(div);
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+  let breakCondition = false;
+  const data = new FormData(Form);
+  Array.from(data.entries()).forEach(item => {
+    const [name, value] = item;
+
+    if(!value || !value.trim().length) {
+      createMessage('Заполните все поля');
+      condition = true;
+    }
+
+    if(name === 'email' && !validation(EMAIL_REGEXP, value)) {
+      createMessage('Введите валидный email, пример: example@gmail.com')
+      condition = true;
+    }
+    if(name === 'tel' && !validation(TEL_REGEXP, value)) {
+      createMessage('Введите валидный номер телефона, пример: +7-999-999-99-99')
+      condition = true;
+    }
+
+
+  });
+  if(breakCondition) {
+    return;
+  }
+  const response = await sendData(data);
+  
+  if(response.status === 200) {
+      createMessage('Данные отправленны успешно');
+  } else {
+      createMessage('Что-то пошло не так')
+  }
+}
+
+async function sendData(data) {
+  return await fetch(mainUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'multipart/form-data' },
+    body: data,
   })
-};
+}
+
+function validation(regexp, value) {
+  return regexp.test(value);
+}
+
+Form.addEventListener('submit', handleFormSubmit)
