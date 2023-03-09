3599b8a4b57338c761c2d5a34210aa725d0319e8
Daniil Belousov <d.belousov.exceedteam@gmail.com>
2023-03-09 14:26:44 +0300

    fix cors

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ async function checkUrls(urls) {
   
     for (const url of urls) {
       try {
-        const response = await fetch(url);
-        results.push({ url, status: response.status });
+        const response = await fetch(url, {
+            method: 'GET',
+            mode: 'no-cors',
+            cache: 'no-cache',
+            headers: {
+                'Access-Control-Allow-Origin' : url
+            }
+        });
+        results.push({ url, error: false, status: response.status });
       } catch (error) {
         results.push({ url, error: true });
       }
     }
-  
+    console.log('results',results);
     return results;
-  }
+  };
+
+  
+
+const arr = ['https://www.google.com/', 'https://www.youtube.com/', 'https://www.ibm.com/developerworks/ru/library/wa-meteor/', 'https://qwerty.qwerty']
+
+checkUrls(arr);
