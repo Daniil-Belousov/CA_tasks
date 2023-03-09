154b2adda11fecbf193aa441af14cefd68a295b6
Daniil Belousov <d.belousov.exceedteam@gmail.com>
2023-03-09 10:54:34 +0300

    tenth_task

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
-const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-const form = document.querySelector('form');
-const message = document.querySelector('#message');
+function getFirstResolved(p1, p2) {
+    return Promise.race([p1, p2]);
+  }
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+const resolvePromise = Promise.resolve("Success 1 promise");
 
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-    const confirm_password = document.querySelector('#confirm_password').value;
+const promise = new Promise(function(resolve) {
+    setTimeout(() => resolve("Success 2 promise"), 1000);
+  });
 
-    const condition = validation(EMAIL_REGEXP, email) && password.length >= 8 && confirm_password === password && password !== password.toLowerCase() && name.trim().length
-
-    if (condition) {
-        const user = { name, email, password };
-        localStorage.setItem('user', JSON.stringify(user));
-
-        message.style.color = 'green';
-        message.textContent = 'Регистрация прошла успешно!';
-        setTimeout(() => message.textContent = '', 3000);
-    } else {
-        message.style.color = 'red';
-        message.textContent = 'Некорректные данные!';
-    }
-});
-
-window.addEventListener('load', function() {
-    const user = JSON.parse(localStorage.getItem('user'));
-
-    if (user) {
-        document.querySelector('#name').value = user.name;
-        document.querySelector('#email').value = user.email;
-        document.querySelector('#password').value = user.password;
-    }
-});
-
-function validation(regexp, value) {
-  return regexp.test(value);
-}
+console.log(getFirstResolved(resolvePromise, promise));
